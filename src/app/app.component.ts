d0a79399ab1a316522a56337fd4272567ecf4bf1
Admin <admin@example.com>
2023-10-07 19:01:12 +0100

    Added card and listing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Album } from './interfaces/Album';
 
 @Component({
   selector: 'ms-root', // app-root
@@ -10,5 +11,34 @@ export class AppComponent {
   topAlbumHeading: string = "Top Albums";
   latestAlbumHeading: string = "Latest Albums";
   bollywoodAlbumHeading: string = "Bollywood Albums";
+
+  topAlbums: Album[] = [
+    {
+      name: "Music 101",
+      albumArt: {
+        thumbnail: "https://via.placeholder.com/150"
+      },
+      price: 100,
+      singers: ["Singer A", "Singer B"],
+      songs: [{
+        duration: 430,
+        name: "Awesome Song",
+        singer: ["Singer A"]
+      }]
+    },
+    {
+      name: "Music 202",
+      albumArt: {
+        thumbnail: "https://via.placeholder.com/150"
+      },
+      price: 200,
+      singers: ["Singer C", "Singer D"],
+      songs: [{
+        duration: 600,
+        name: "Great Song",
+        singer: ["Singer C"]
+      }]
+    }
+  ]
 
 }
--- src/app/components/album-listing/album-listing.component.ts
+++ src/app/components/album-listing/album-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Album } from 'src/app/interfaces/Album';
 
 @Component({
   selector: 'ms-album-listing',
@@ -8,5 +9,6 @@ import { Component, Input } from '@angular/core';
 export class AlbumListingComponent {
 
   @Input() heading: string = "";
+  @Input() albums: Album[] = [];
 
 }
