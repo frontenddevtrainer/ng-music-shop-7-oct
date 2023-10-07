a08efe073513bbbf0b05193fcb2a76895a14ebb4
Admin <admin@example.com>
2023-10-07 18:17:42 +0100

    Intial setup with header component.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ms-root', // app-root
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'music-shop';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
