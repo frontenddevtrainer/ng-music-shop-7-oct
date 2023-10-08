df8caa0cdd00fb487c6b6d712ecc1209b21bdd54
Admin <admin@example.com>
2023-10-08 17:54:21 +0100

    Added service for Albums

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Album } from './interfaces/Album';
+import { AlbumsService } from './services/albums.service';
 
 @Component({
   selector: 'ms-root', // app-root
@@ -8,52 +8,16 @@ import { Album } from './interfaces/Album';
 })
 export class AppComponent {
 
+  constructor(private _albums: AlbumsService) {
+
+  }
+
   topAlbumHeading: string = "Top Albums";
   latestAlbumHeading: string = "Latest Albums";
   bollywoodAlbumHeading: string = "Bollywood Albums";
 
-  topAlbums: Album[] = [
-    {
-      name: "Music 101",
-      albumArt: {
-        thumbnail: "https://via.placeholder.com/150"
-      },
-      price: 100,
-      singers: ["Singer A", "Singer B"],
-      songs: [{
-        duration: 430,
-        name: "Awesome Song",
-        singer: ["Singer A"]
-      }],
-      isPurchasedByUser: true,
-    },
-    {
-      name: "Music 202",
-      albumArt: {
-        thumbnail: "https://via.placeholder.com/150"
-      },
-      price: 200,
-      singers: ["Singer C", "Singer D"],
-      songs: [{
-        duration: 600,
-        name: "Great Song",
-        singer: ["Singer C"]
-      }],
-      newRelesedDate: '2023-10-08T16:13:46.338Z'
-    },
-    {
-      name: "Music 303",
-      albumArt: {
-        thumbnail: "https://via.placeholder.com/150"
-      },
-      price: 200000,
-      singers: ["Singer D", "Singer E"],
-      songs: [{
-        duration: 549,
-        name: "Great Song",
-        singer: ["Singer C"]
-      }],
-    }
-  ]
+  albums = this._albums.topAlbums
+
+
 
 }
