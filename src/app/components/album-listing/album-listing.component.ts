8e7a104d9386384062fdcd4ffc38a1d7ac11d504
Admin <admin@example.com>
2023-10-14 17:29:40 +0100

    Added animation using stagger on the album card.

--- src/app/components/album-listing/album-listing.component.ts
+++ src/app/components/album-listing/album-listing.component.ts
@@ -1,10 +1,27 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { Album } from 'src/app/interfaces/Album';
 
 @Component({
   selector: 'ms-album-listing',
   templateUrl: './album-listing.component.html',
-  styleUrls: ['./album-listing.component.scss']
+  styleUrls: ['./album-listing.component.scss'],
+  animations: [
+    trigger("listAnimation", [
+      transition("* <=> *", [
+        query(":enter", [
+          style({ opacity: 0, transform: "translateX(-40px)" }),
+          stagger('200ms', [
+            animate('300ms', style({ opacity: 1, transform: "none" })),
+            // animate('50ms', style({ opacity: 0 })),
+            // animate('50ms', style({ opacity: 1 })),
+            // animate('50ms', style({ opacity: 0 })),
+            // animate('50ms', style({ opacity: 1 }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class AlbumListingComponent {
 
